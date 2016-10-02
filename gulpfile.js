afe48a5cc3e0623f66300b0f8382385265bc6d9a
Kevin Wirth <me@wirthdesign.com>
2016-10-02 10:52:10 -0400

    Added LiveReload to the gulp functions.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    livereload = require('gulp-livereload');
 
 gulp.task('sass', function () {
   return gulp.src('sass/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest(''));
+    .pipe(gulp.dest(''))
+    .pipe(livereload());
 });
 
 gulp.task('sass:watch', function () {
+  livereload.listen();
   gulp.watch('sass/**/*.scss', ['sass']);
 });
