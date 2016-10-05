ab141a0b259b986c96ce55139b5710de9cf33dda
Kevin Wirth <me@wirthdesign.com>
2016-10-05 14:45:59 -0400

    Added GA back into the header.

--- header.php
+++ header.php
@@ -2,14 +2,24 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
   <title><?php bloginfo( 'name' ); ?></title>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link href="<?php bloginfo('stylesheet_url'); ?>" media="screen" rel="stylesheet" />
+  <link href="<?php bloginfo('stylesheet_url'); ?>" media="screen" rel="stylesheet" />
   <link href="<?php bloginfo('template_url'); ?>/favicon.ico" rel="icon" type="image/png" />
 
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-64003940-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+	
   <script src="<?php bloginfo('template_url'); ?>/js/meetup.js"></script>
 </head>
 
