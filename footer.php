910608f00c2b676400cb9689cdbc222f39e596b7
Kevin Wirth <me@wirthdesign.com>
2016-12-02 21:40:08 -0500

    Created a better footer for the website

--- footer.php
+++ footer.php
@@ -1,18 +1,26 @@
 <div class="container contact-us--wrapper module--tertiary clearfix">
   <div class="content">
+
     <h3>Contact Us</h3>
     <?php echo do_shortcode( '[contact-form-7 title="Contact Us"]' ); ?>
-
-    <div class="size-4 text--align-right fr">
-      <a href="" class="icon icon--twitter" target="_blank">Twitter</a>
-      <a href="" class="icon icon--github" target="_blank">GitHub</a>
-      <a href="" class="icon icon--facebook" target="_blank">Facebook</a>
-      <!-- <a href="" class="icon icon--email" target="_blank">Email</a> -->
-    </div>
 
   </div>
 </div>
 
+<div class="footer">
+  <div class="content">
+    <div class="size-8 fl">
+      <small>AkronWIT | 216-694-8339 | <a href="mailto:info@akronwit.org">info@akronwit.org</a></small>
+    </div>
+    <div class="size-4 fr text--align-right">
+      <a href="http://twitter.com/akronwit" class="icon icon--twitter" target="_blank">Twitter</a>
+      <a href="http://github.com/akronwit" class="icon icon--github" target="_blank">GitHub</a>
+      <a href="http://www.facebook.com/AkronWiT/" class="icon icon--facebook" target="_blank">Facebook</a>
+    </div>
+  </div>
+</div>
+
 <?php wp_footer(); ?>
+
 </body>
 </html>
