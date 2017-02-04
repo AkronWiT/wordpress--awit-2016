da7ba9d4e4a214e557e274597fd7fa8fdb1079a6
Kevin Wirth <fire@brethr.com>
2017-02-04 15:51:15 -0500

    Added WooCommerce function to theme and product template

--- functions.php
+++ functions.php
@@ -32,3 +32,13 @@ function custom_excerpt_length( $length ) {
 	return 32;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+
+////////////////////////////////////////////////////////////////////////////////
+// WooCommerce Support
+////////////////////////////////////////////////////////////////////////////////
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
